66fdfe2878c2191b59d7b0088bd90f3044f6cd74
YBS01 <103018325+YBS01@users.noreply.github.com>
2024-04-12 02:40:07 -0500

    wkngStill

--- backend/server.js
+++ backend/server.js
@@ -83,6 +83,9 @@ io.on("connection", (socket) => {
   });
   socket.on("typing", (room) => socket.in(room).emit("typing"));
   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+  socket.on("cue status update:", (messageId, status) => {
+    socket.in(messageId).emit("cue status update", { messageId, status });
+  })
 
   socket.on("new message", (newMessageRecieved) => {
     var chat = newMessageRecieved.chat;
